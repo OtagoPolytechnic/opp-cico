db7de76a02e8d14b2d5737bd46ca10a83b0d65ac
AstinT <Astin8@hotmail.co.nz>
2016-08-11 11:57:58 +1200

    Added static homepage

--- cico/config/routes.rb
+++ cico/config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :users
   resources :items
   resources :item_types
+  get "/pages/:page" => "pages#show"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root "pages#show", page: "home"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
