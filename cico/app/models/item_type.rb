be7b5954da5b01afc5de805b9ce3095ec203898c
AstinT <Astin8@hotmail.co.nz>
2016-10-17 11:08:53 +1300

    Added validations

--- cico/app/models/item_type.rb
+++ cico/app/models/item_type.rb
@@ -1,3 +1,12 @@
 class ItemType < ActiveRecord::Base
+	
 	has_many :items
+
+	# Validations
+	validates_presence_of :name
+	validates_length_of :name, :maximum => 50
+
+	validates_presence_of :description
+	validates_length_of :description, :maximum => 255
+
 end
