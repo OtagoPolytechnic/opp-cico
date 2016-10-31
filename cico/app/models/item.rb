c88513a0bafab9e6469ae41a90b203727262a454
AstinT <Astin8@hotmail.co.nz>
2016-11-01 12:27:52 +1300

    Added item validations

--- cico/app/models/item.rb
+++ cico/app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ActiveRecord::Base
+	
 	has_one :item_type
 	has_many :checkouts
+
+	# Validations
+	validates_presence_of :name
+	validates_length_of :name, :maximum => 50
+
+	validates_presence_of :price
+
+	validates_presence_of :item_type_id
+
 end
