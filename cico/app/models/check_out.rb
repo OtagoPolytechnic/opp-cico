eac22e7cbad41a8c955bd18deeb941d98190dc83
AstinT <Astin8@hotmail.co.nz>
2016-11-04 11:33:15 +1300

    Updated models

--- cico/app/models/check_out.rb
+++ cico/app/models/check_out.rb
@@ -1,4 +1,8 @@
 class CheckOut < ActiveRecord::Base
+	
 	has_many :items
 	has_many :users
+
+	
+	
 end
--- cico/app/models/item_type.rb
+++ cico/app/models/item_type.rb
@@ -2,6 +2,12 @@ class ItemType < ActiveRecord::Base
 	
 	has_many :items
 
+	# Scopes
+	scope :retired, lambda { where.not(:retired_at => nil) }
+	scope :not_retired, lambda { where(:retired_at => nil) }
+	scope :oldest_first, lambda { order("item_types.created_at ASC") }
+	scope :newest_first, lambda { order("item_types.created_at DESC") }
+
 	# Validations
 	validates_presence_of :name
 	validates_length_of :name, :maximum => 50
